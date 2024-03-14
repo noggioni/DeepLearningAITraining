bf6f776c60748906357f9d379682dd55bfec0766
Nicolas Oggioni <n.oggioni@sap.com>
2024-03-13 22:33:56 -0400

    Create 4.QestionsAnswers.py

---
+++ LangChain_for_LLM/4.QestionsAnswers.py
@@ -0,0 +1,45 @@
+import os
+
+from dotenv import load_dotenv, find_dotenv
+_ = load_dotenv(find_dotenv()) # read local .env file
+
+# account for deprecation of LLM model
+import datetime
+# Get the current date
+current_date = datetime.datetime.now().date()
+
+# Define the date after which the model should be set to "gpt-3.5-turbo"
+target_date = datetime.date(2024, 6, 12)
+
+# Set the model variable based on the current date
+if current_date > target_date:
+    llm_model = "gpt-3.5-turbo"
+else:
+    llm_model = "gpt-3.5-turbo-0301"
+
+from langchain.chains import RetrievalQA
+from langchain.chat_models import ChatOpenAI
+from langchain.document_loaders import CSVLoader
+from langchain.vectorstores import DocArrayInMemorySearch
+from IPython.display import display, Markdown
+from langchain.llms import OpenAI
+
+file = 'OutdoorClothingCatalog_1000.csv'
+loader = CSVLoader(file_path=file)
+
+from langchain.indexes import VectorstoreIndexCreator
+
+index = VectorstoreIndexCreator(
+    vectorstore_cls=DocArrayInMemorySearch
+).from_loaders([loader])
+
+query ="Please list all your shirts with sun protection \
+in a table in markdown and summarize each one."
+
+llm_replacement_model = OpenAI(temperature=0, 
+                               model='gpt-3.5-turbo-instruct')
+
+response = index.query(query, 
+                       llm = llm_replacement_model)
+
+display(Markdown(response))

b931d8898ab65cc4c3bd15788e7551120715f2ff
Nicolas Oggioni <n.oggioni@sap.com>
2024-03-13 22:53:04 -0400

    Step

--- LangChain_for_LLM/4.QestionsAnswers.py
+++ LangChain_for_LLM/4.QestionsAnswers.py
@@ -43,3 +43,46 @@ response = index.query(query,
                        llm = llm_replacement_model)
 
 display(Markdown(response))
+
+## Step By Step
+
+from langchain.document_loaders import CSVLoader
+loader = CSVLoader(file_path=file)
+
+docs = loader.load()
+
+docs[0]
+
+retriever = db.as_retriever()
+
+llm = ChatOpenAI(temperature = 0.0, model=llm_model)
+
+qdocs = "".join([docs[i].page_content for i in range(len(docs))])
+
+response = llm.call_as_llm(f"{qdocs} Question: Please list all your \
+shirts with sun protection in a table in markdown and summarize each one.") 
+
+display(Markdown(response))
+
+qa_stuff = RetrievalQA.from_chain_type(
+    llm=llm, 
+    chain_type="stuff", 
+    retriever=retriever, 
+    verbose=True
+)
+
+query =  "Please list all your shirts with sun protection in a table \
+in markdown and summarize each one."
+
+response = qa_stuff.run(query)
+
+display(Markdown(response))
+
+response = index.query(query, llm=llm)
+
+index = VectorstoreIndexCreator(
+    vectorstore_cls=DocArrayInMemorySearch,
+    embedding=embeddings,
+).from_loaders([loader])
+
+

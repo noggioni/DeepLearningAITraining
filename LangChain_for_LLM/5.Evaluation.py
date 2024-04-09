200a13189ee6be671b912056ca398aebee87d824
Nicolas Oggioni <n.oggioni@sap.com>
2024-04-09 19:11:59 -0400

    Simple Evaluation

---
+++ LangChain_for_LLM/5.Evaluation.py
@@ -0,0 +1,80 @@
+## LangChain: Evaluation
+import os
+
+from dotenv import load_dotenv, find_dotenv
+_ = load_dotenv(find_dotenv()) # read local .env file
+
+# account for deprecation of LLM model
+import datetime
+# Get the current date
+current_date = datetime.datetime.now().date()
+
+# Define the date after which the model should be set to "gpt-3.5-turbo"
+target_date = datetime.date(2024, 6, 12)
+
+# Set the model variable based on the current date
+if current_date > target_date:
+    llm_model = "gpt-3.5-turbo"
+else:
+    llm_model = "gpt-3.5-turbo-0301"
+
+from langchain.chains import RetrievalQA
+from langchain.chat_models import ChatOpenAI
+from langchain.document_loaders import CSVLoader
+from langchain.indexes import VectorstoreIndexCreator
+from langchain.vectorstores import DocArrayInMemorySearch
+
+file = 'OutdoorClothingCatalog_1000.csv'
+loader = CSVLoader(file_path=file)
+data = loader.load()
+
+index = VectorstoreIndexCreator(
+    vectorstore_cls=DocArrayInMemorySearch
+).from_loaders([loader])
+
+llm = ChatOpenAI(temperature = 0.0, model=llm_model)
+qa = RetrievalQA.from_chain_type(
+    llm=llm, 
+    chain_type="stuff", 
+    retriever=index.vectorstore.as_retriever(), 
+    verbose=True,
+    chain_type_kwargs = {
+        "document_separator": "<<<<>>>>>"
+    }
+)
+
+data[10]
+
+data[11]
+
+examples = [
+    {
+        "query": "Do the Cozy Comfort Pullover Set\
+        have side pockets?",
+        "answer": "Yes"
+    },
+    {
+        "query": "What collection is the Ultra-Lofty \
+        850 Stretch Down Hooded Jacket from?",
+        "answer": "The DownTek collection"
+    }
+]
+
+from langchain.evaluation.qa import QAGenerateChain
+
+example_gen_chain = QAGenerateChain.from_llm(ChatOpenAI(model=llm_model))
+
+new_examples = example_gen_chain.apply_and_parse(
+    [{"doc": t} for t in data[:5]]
+)
+
+new_examples[0]
+
+data[0]
+
+examples += new_examples
+
+qa.run(examples[0]["query"])
+
+
+

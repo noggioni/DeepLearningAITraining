278718b800feb9424281ee70ce01f33a3a9f7044
Nicolas Oggioni <n.oggioni@sap.com>
2024-04-12 10:52:19 -0400

    Python Agent

--- LangChain_for_LLM/6.Agents.py
+++ LangChain_for_LLM/6.Agents.py
@@ -41,3 +41,42 @@ agent= initialize_agent(
     verbose = True)
 
 agent("What is the square root of 300?")
+
+## Wiki Example
+
+question = "Tom M. Mitchell is an American computer scientist \
+and the Founders University Professor at Carnegie Mellon University (CMU)\
+what book did he write?"
+
+result = agent(question) 
+
+## Python Agent
+
+agent = create_python_agent(
+    llm,
+    tool=PythonREPLTool(),
+    verbose=True
+)
+
+customer_list = [["Harrison", "Chase"], 
+                 ["Lang", "Chain"],
+                 ["Dolly", "Too"],
+                 ["Elle", "Elem"], 
+                 ["Geoff","Fusion"], 
+                 ["Trance","Former"],
+                 ["Jen","Ayai"]
+                ]
+
+agent.run(f"""Sort these customers by \
+last name and then first name \
+and print the output: {customer_list}""") 
+
+import langchain
+langchain.debug=True
+agent.run(f"""Sort these customers by \
+last name and then first name \
+and print the output: {customer_list}""") 
+langchain.debug=False
+
+
+

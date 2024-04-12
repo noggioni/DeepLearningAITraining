3151095ca5171b7f3ad423171e39e3d1570924be
Nicolas Oggioni <n.oggioni@sap.com>
2024-04-12 10:39:12 -0400

    Create 6.Agents.py

---
+++ LangChain_for_LLM/6.Agents.py
@@ -0,0 +1,43 @@
+import os
+
+from dotenv import load_dotenv, find_dotenv
+_ = load_dotenv(find_dotenv()) # read local .env file
+
+import warnings
+warnings.filterwarnings("ignore")
+
+# account for deprecation of LLM model
+import datetime
+# Get the current date
+current_date = datetime.datetime.now().date()
+
+# Define the date after which the model should be set to "gpt-3.5-turbo"
+target_date = datetime.date(2024, 6, 12)
+
+# Set the model variable based on the current date
+if current_date > target_date:
+    llm_model = "gpt-3.5-turbo"
+else:
+    llm_model = "gpt-3.5-turbo-0301"
+
+#!pip install -U wikipedia
+
+ from langchain.agents.agent_toolkits import create_python_agent
+from langchain.agents import load_tools, initialize_agent
+from langchain.agents import AgentType
+from langchain.tools.python.tool import PythonREPLTool
+from langchain.python import PythonREPL
+from langchain.chat_models import ChatOpenAI
+
+llm = ChatOpenAI(temperature=0, model=llm_model)
+
+tools = load_tools(["llm-math","wikipedia"], llm=llm)
+
+agent= initialize_agent(
+    tools, 
+    llm, 
+    agent=AgentType.CHAT_ZERO_SHOT_REACT_DESCRIPTION,
+    handle_parsing_errors=True,
+    verbose = True)
+
+agent("What is the square root of 300?")

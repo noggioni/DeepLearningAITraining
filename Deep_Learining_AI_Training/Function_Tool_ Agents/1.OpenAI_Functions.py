e2483c1ab0924df96ff3f19e4c6a3397e7440d8e
Nicolas Oggioni <n.oggioni@sap.com>
2024-09-16 12:02:37 -0400

    Create 1.OpenAI_Functions.py

---
+++ Deep_Learining_AI_Training/Function_Tool_ Agents/1.OpenAI_Functions.py
@@ -0,0 +1,55 @@
+import os
+import openai
+
+from dotenv import load_dotenv, find_dotenv
+_ = load_dotenv(find_dotenv()) # read local .env file
+openai.api_key = os.environ['OPENAI_API_KEY']
+
+import json
+
+# Example dummy function hard coded to return the same weather
+# In production, this could be your backend API or an external API
+def get_current_weather(location, unit="fahrenheit"):
+    """Get the current weather in a given location"""
+    weather_info = {
+        "location": location,
+        "temperature": "72",
+        "unit": unit,
+        "forecast": ["sunny", "windy"],
+    }
+    return json.dumps(weather_info)
+
+# define a function
+functions = [
+    {
+        "name": "get_current_weather",
+        "description": "Get the current weather in a given location",
+        "parameters": {
+            "type": "object",
+            "properties": {
+                "location": {
+                    "type": "string",
+                    "description": "The city and state, e.g. San Francisco, CA",
+                },
+                "unit": {"type": "string", "enum": ["celsius", "fahrenheit"]},
+            },
+            "required": ["location"],
+        },
+    }
+]
+
+messages = [
+    {
+        "role": "user",
+        "content": "What's the weather like in Boston?"
+    }
+]
+
+import openai
+
+response = openai.ChatCompletion.create(
+    model="gpt-3.5-turbo-0613",
+    messages=messages,
+    functions=functions
+)
+

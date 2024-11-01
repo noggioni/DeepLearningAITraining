7ac4d03ec73b21cff82d7d513a9487f4ad58f5a0
Nicolas Oggioni <nicolas.oggioni@gmail.com>
2024-11-01 16:40:24 -0400

    test

--- Deep_Learining_AI_Training/ChatWithYourDATA/1.DocumentLoading.py
+++ Deep_Learining_AI_Training/ChatWithYourDATA/1.DocumentLoading.py
@@ -3,8 +3,8 @@ import openai
 import sys
 sys.path.append('../..')
 
-from dotenv import load_dotenv, find_dotenv
-_ = load_dotenv(find_dotenv()) # read local .env file
+# from dotenv import load_dotenv, find_dotenv
+#_ = load_dotenv(find_dotenv()) # read local .env file
 
 openai.api_key  = os.environ['OPENAI_API_KEY']
 

6f1c0b5eaaf8b381e81a9b11e4fbf6fabf2d656f
Nicolas Oggioni <nicolas.oggioni@gmail.com>
2024-11-14 18:50:59 -0500

    Update 3.OutreachCampaign.py

---
+++ Deep_Learining_AI_Training/MultiAI_Agent_CrewAI/3.OutreachCampaign.py
@@ -0,0 +1,15 @@
+# Warning control
+import warnings
+warnings.filterwarnings('ignore')
+
+from crewai import Agent, Task, Crew
+
+import os
+# from utils import get_openai_api_key
+
+openai_api_key=os.getenv('OPENAI_API_KEY')
+
+# openai_api_key = get_openai_api_key()
+os.environ["OPENAI_MODEL_NAME"] = 'gpt-3.5-turbo'
+
+## Creating Agents
